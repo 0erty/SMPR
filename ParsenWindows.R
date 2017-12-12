30d43e7a662a4b9851469e94df9a5ea780262a99
Edem Ibragimov <32879454+0erty@users.noreply.github.com>
2017-12-12 21:43:12 +0300

    Update ParsenWindows.R

--- ParsenWindows.R
+++ ParsenWindows.R
@@ -8,34 +8,14 @@ parcenWindowFixed <- function(xl,z,h){
   for(i in 1:length(list)){
     counts[i] = 0
   }
+  #Вычисляем сумму весов объектов
   for (i in 1:l)
   {
     counts[which(xl[i,n+1]==list)] =
       counts[which(xl[i,n+1]==list)] + kernel(euclideanDistance(z,xl[i,1:n])/h)
   }
   print(counts)
-  return (list[which.max(counts)]) 
-  
-}
-parcenWindowFloat <- function(xl,z,k){
-  orderedXl <- sortObjectsByDist(xl, z)
-  l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1          
-  list <- unique(xl[,n+1])
-  h <- euclideanDistance(z,orderedXl[k+1,1:n])
-  counts = 0
-  for(i in 1:length(list)){
-    counts[i] = 0
-  }
-  
-#Вычисляем сумму весов объектов
-  for (i in 1:l)
-  {
-    counts[which(xl[i,n+1]==list)] =
-      counts[which(xl[i,n+1]==list)] + kernel(euclideanDistance(z,xl[i,1:n])/h)
-  }
-  print(counts)
-# Класс с максимальным весом  
+  # Класс с максимальным весом 
   return (list[which.max(counts)]) 
   
 }
@@ -45,6 +25,6 @@ plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species
 z <- c(2.7, 1) 
 xl <- iris[, 3:5] 
 class <- parcenWindowFloat(xl, z, 8)
-res <- loo(xl,5,parcenWindowFloat)
+res <- loo(xl,5,parcenWindowFixed)
 print(res)
 points(z[1], z[2], pch = 22, bg = colors[class], asp = 1) 
